23af5a54c92f069d21c240bc599ed698c914687c
nurulamin092 <mnurulamin092@gmial.com>
2022-08-22 10:14:05 +0600

    todos action and action type added

---
+++ src/redux/todos/action.js
@@ -0,0 +1,43 @@
+import { ADDED, ALLCOMPLETED, CLEARCOMPLETED, COLORESELECTED, DELETED, TOGGLED } from "./actionType"
+
+export const added = (todoText) => {
+    return {
+        type: ADDED,
+        payload: todoText
+    }
+};
+
+export const toggle = (todoId) => {
+    return {
+        type: TOGGLED,
+        payload: todoId
+    }
+};
+
+export const colorSelected = (todoId, color) => {
+    return {
+        type: COLORESELECTED,
+        payload: {
+            todoId, color
+        }
+    }
+};
+
+export const deleted = (todoId) => {
+    return {
+        type: DELETED,
+        payload: todoId
+    }
+};
+
+export const allCompleted = () => {
+    return {
+        type: ALLCOMPLETED
+    }
+}
+
+export const clearCompleted = () => {
+    return {
+        type: CLEARCOMPLETED
+    }
+}
